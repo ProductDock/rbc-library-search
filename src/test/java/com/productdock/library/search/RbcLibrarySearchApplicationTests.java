d2f2569fe507cbc55341dccf2fa3baa264bf4cda
Dusan <dusan.petrovic@productdock.com>
2022-04-11 14:40:48 +0200

    Added initial spring boot project

---
+++ src/test/java/com/productdock/library/search/RbcLibrarySearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productdock.library.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbcLibrarySearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
