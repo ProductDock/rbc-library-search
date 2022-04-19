8e113c02ebee6dfe5bf0f6ab661114ecdc5d5eb2
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-19 13:26:44 +0200

    added CI/CD workflow

---
+++ prod-env/update-search-image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo usermod -a -G docker ${USER}
+docker-credential-gcr configure-docker
+
+docker stop rbc-library-search
+docker rm rbc-library-search
+docker rmi $(docker images | grep "rbc-library-search")
+
+docker run -dp 8080:8080 --name=rbc-library-search gcr.io/prod-pd-library/rbc-library-search:$1
+docker container ls -a
