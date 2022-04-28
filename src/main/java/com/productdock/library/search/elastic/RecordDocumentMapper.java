57d0bb1b0e1b95f3d268dac5c8fdf72fe106e53f
Natasa Tutus <natasa.tutus@productdock.com>
2022-04-28 11:23:03 +0200

    renamed RecordMapper to RecordDocumentMapper

--- src/main/java/com/productdock/library/search/elastic/RecordDocumentMapper.java
+++ src/main/java/com/productdock/library/search/elastic/RecordDocumentMapper.java
@@ -1,5 +1,6 @@
-package com.productdock.library.search.elastic.document;
+package com.productdock.library.search.elastic;
 
+import com.productdock.library.search.elastic.document.Record;
 import com.productdock.library.search.kafka.cosumer.messages.BookRecord;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
@@ -7,7 +8,7 @@ import org.mapstruct.ReportingPolicy;
 import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
-public interface RecordMapper {
+public interface RecordDocumentMapper {
 
     Record toRecord(BookRecord bookRecord);
 
