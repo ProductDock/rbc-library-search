f71c4b38529f30a5d2b7a41ac7c1a52247704798
Dusan <dusan.petrovic@productdock.com>
2022-05-17 14:09:24 +0200

    BookDto returns book rating

--- src/main/java/com/productdock/library/search/book/BookDto.java
+++ src/main/java/com/productdock/library/search/book/BookDto.java
@@ -9,10 +9,17 @@ public class BookDto {
     public String author;
     public String cover;
     public List<Record> records;
+    public Rating rating = new Rating();
 
     public static class Record {
 
         public String email;
         public BookStatus status;
+    }
+
+    public static class Rating {
+
+        public Double score;
+        public Integer count;
     }
 }
