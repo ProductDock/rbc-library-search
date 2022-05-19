26c639ca164f12eeeb51669743e219626b3517a2
NenadBecanovicPD <100684660+NenadBecanovicPD@users.noreply.github.com>
2022-05-19 11:05:26 +0200

    Update prod-env script

--- prod-env/update-search-image.sh
+++ prod-env/update-search-image.sh
@@ -6,5 +6,5 @@ docker stop rbc-library-search
 docker rm rbc-library-search
 docker rmi $(docker images | grep "rbc-library-search")
 
-docker run -dp 8081:8080 --env-file /home/pd-library/.search-service_env --name=rbc-library-search gcr.io/prod-pd-library/rbc-library-search:$1
+docker run -dp 8081:8080 -e KAFKA_SERVER_URL=$KAFKA_SERVER_URL --env-file /home/pd-library/.search-service_env --name=rbc-library-search gcr.io/prod-pd-library/rbc-library-search:$1
 docker container ls -a
