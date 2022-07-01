2e10effd777d08fd7d71e2bf527121d595e0f550
Milica Zivkov <milica.zivkov@productdock.com>
2022-07-01 15:20:51 +0200

    explain behaviour of kafka producer in tests

--- src/test/java/com/productdock/library/search/data/provider/KafkaTestProducer.java
+++ src/test/java/com/productdock/library/search/data/provider/KafkaTestProducer.java
@@ -2,11 +2,16 @@ package com.productdock.library.search.data.provider;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 @Component
+@Slf4j
 public class KafkaTestProducer {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
@@ -16,6 +21,19 @@ public class KafkaTestProducer {
 
     public void send(String topic, Object payload) throws JsonProcessingException {
         String message = OBJECT_MAPPER.writeValueAsString(payload);
-        kafkaTemplate.send(topic, message);
+        ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send(topic, message);
+        ListenableFutureCallback<? super SendResult<String, String>> callb = new ListenableFutureCallback<SendResult<String, String>>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.info("PRODUCER FAILED - topic {} message {} ", topic, message);
+                log.error("PRODUCER FAILED", ex.getStackTrace());
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("PRODUCER SUCCESS - topic {} message {} ", topic, message);
+            }
+        };
+        send.addCallback(callb);
     }
 }
