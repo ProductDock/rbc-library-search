32ae5ac6ef4200cabe6c24b14a10293249df6cc7
Milica Zivkov <milica.zivkov@productdock.com>
2022-07-04 10:39:30 +0200

    make Kafka Test Producer block thread until it sends message to Kafka

--- src/test/java/com/productdock/library/search/data/provider/KafkaTestProducer.java
+++ src/test/java/com/productdock/library/search/data/provider/KafkaTestProducer.java
@@ -2,6 +2,7 @@ package com.productdock.library.search.data.provider;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -9,6 +10,8 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.ExecutionException;
 
 @Component
 @Slf4j
@@ -19,7 +22,8 @@ public class KafkaTestProducer {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    public void send(String topic, Object payload) throws JsonProcessingException {
+    @SneakyThrows
+    public SendResult<String, String> send(String topic, Object payload) {
         String message = OBJECT_MAPPER.writeValueAsString(payload);
         ListenableFuture<SendResult<String, String>> send = kafkaTemplate.send(topic, message);
         ListenableFutureCallback<? super SendResult<String, String>> callb = new ListenableFutureCallback<SendResult<String, String>>() {
@@ -35,5 +39,6 @@ public class KafkaTestProducer {
             }
         };
         send.addCallback(callb);
+        return send.get();
     }
 }
